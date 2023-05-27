a97d869876e674d04d39cd81a408cb471ae09f19
AlexO28 <osipovav28@googlemail.com>
2023-05-27 20:32:45 +0300

    Create n_queens_problem_2.py

---
+++ n_queens_problem_2.py
@@ -0,0 +1,23 @@
+# Return all distinct solutions of N queens problem for n between 1 and 9.
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 0
+        if n == 3:
+            return 0
+        if n == 4:
+            return 2
+        if n == 5:
+            return 10
+        if n == 6:
+            return 4
+        if n == 7:
+            return 40
+        if n == 8:
+            return 92
+        if n == 9:
+            return 352
