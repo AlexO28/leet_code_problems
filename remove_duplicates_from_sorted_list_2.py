fdeb6cc6022e0c7c541e80072190674fa8cfc130
AlexO28 <osipovav28@googlemail.com>
2023-06-18 11:49:20 +0300

    Update remove_duplicates_from_sorted_list_2.py
    Add task description

--- remove_duplicates_from_sorted_list_2.py
+++ remove_duplicates_from_sorted_list_2.py
@@ -1,3 +1,5 @@
+# Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
