f4c972faf49b6c62aca3d588dd78bd28ba7334ca
AlexO28 <osipovav28@googlemail.com>
2023-07-05 16:00:50 +0300

    Create flatten_binary_tree_to_linked_list.py

---
+++ flatten_binary_tree_to_linked_list.py
@@ -0,0 +1,25 @@
+# Given the root of a binary tree, flatten the tree into a "linked list":
+# The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+# The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        while root:
+            if root.left:
+                pre = root.left
+                while pre.right:
+                    pre = pre.right
+                pre.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
