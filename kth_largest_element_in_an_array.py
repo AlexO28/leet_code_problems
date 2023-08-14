b58298e823356d02d658d10fb330d6d5c506f679
AlexO28 <osipovav28@googlemail.com>
2023-08-14 23:01:48 +0300

    Create kth_largest_element_in_an_array.py

---
+++ kth_largest_element_in_an_array.py
@@ -0,0 +1,8 @@
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element. 
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort(reverse = True)
+        return nums[k-1]
