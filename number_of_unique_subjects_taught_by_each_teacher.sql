ce2788e654df2ad7fb74e1f42b03acd59267fae0
AlexO28 <osipovav28@googlemail.com>
2023-09-09 21:45:05 +0300

    Create number_of_unique_subjects_taught_by_each_teacher.sql

---
+++ number_of_unique_subjects_taught_by_each_teacher.sql
@@ -0,0 +1,3 @@
+--Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+select teacher_id, count(distinct subject_id) as cnt from Teacher group by teacher_id
