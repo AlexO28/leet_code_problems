4ff979ab6c8441e885349466c896208ee3812007
AlexO28 <osipovav28@googlemail.com>
2023-09-10 16:26:27 +0300

    Create product_sales_analysis_3.sql

---
+++ product_sales_analysis_3.sql
@@ -0,0 +1,8 @@
+--Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+# Write your MySQL query statement below
+
+select product_id, year as first_year, quantity, price from Sales
+    where (product_id, year) in (
+        select product_id, min(year) from Sales
+        group by product_id)
