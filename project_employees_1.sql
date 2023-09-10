0885c243c78862af679874d8508eaf5a394aae93
AlexO28 <osipovav28@googlemail.com>
2023-09-10 16:43:57 +0300

    Create project_employees_1.sql

---
+++ project_employees_1.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select project_id, ifnull(round(avg(experience_years), 2), 0) as average_years
+    from Project left join Employee
+    on Project.employee_id = Employee.employee_id
+    group by project_id
