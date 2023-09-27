9eb2e3a304d9472810b715ff2192b3c1a2e5f656
Alexey Osipov <osipovav28@googlemail.com>
2023-09-27 22:11:26 +0300

    Create invalid_tweets.sql

---
+++ invalid_tweets.sql
@@ -0,0 +1,5 @@
+/* Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.*/
+
+/* Write your T-SQL query statement below */
+
+select tweet_id from Tweets where len(content) > 15
