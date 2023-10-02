44b1b4add3898a942d94174f216a171c4a89877f
Alexey Osipov <osipovav28@googlemail.com>
2023-10-02 18:12:23 +0300

    Create recyclable_and_low_fat_products.sql

---
+++ recyclable_and_low_fat_products.sql
@@ -0,0 +1,6 @@
+--Write a solution to find the ids of products that are both low fat and recyclable.
+
+/* Write your T-SQL query statement below */
+
+select product_id from Products where
+low_fats = 'Y' and recyclable = 'Y' 
