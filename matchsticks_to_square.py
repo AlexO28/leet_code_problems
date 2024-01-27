917ccef0f7a4ccc82f19223708abd9405865f135
Alexey Osipov <osipovav28@googlemail.com>
2024-01-27 10:14:55 +0300

    Update matchsticks_to_square.py

--- matchsticks_to_square.py
+++ matchsticks_to_square.py
@@ -1,3 +1,5 @@
+# You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. You want to use all the matchsticks to make one square. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+# Return true if you can make this square and false otherwise.
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
         if len(matchsticks) < 4:
