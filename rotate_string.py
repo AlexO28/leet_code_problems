95b2eb38de3877d49ff29294274516e2e6c577e9
Alexey Osipov <osipovav28@googlemail.com>
2024-06-20 19:55:24 +0300

    Create rotate_string.py

---
+++ rotate_string.py
@@ -0,0 +1,12 @@
+# Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+# A shift on s consists of moving the leftmost character of s to the rightmost position.
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(s)-1):
+            if i == 0:
+                new_s = s
+            else:
+                new_s = s[(i+1):] + s[:(i+1)]
+            if new_s == goal:
+                return True
+        return False
