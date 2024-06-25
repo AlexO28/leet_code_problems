77132c216a892195a8b852dd49a1837e767e2053
Alexey Osipov <osipovav28@googlemail.com>
2024-06-25 09:02:18 +0300

    Create binary_search_to_greater_sum_tree.py

---
+++ binary_search_to_greater_sum_tree.py
@@ -0,0 +1,21 @@
+# Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.modifyNodes(root, 0)
+        return root
+
+    def modifyNodes(self, tree, val_above):
+        if tree.right is not None:
+            val_right = self.modifyNodes(tree.right, val_above)
+            tree.val += val_right
+        else:
+            tree.val += val_above
+        if tree.left is not None:
+            return self.modifyNodes(tree.left, tree.val)
+        return tree.val
