70ecbe48ad49ff4d76593c580b7e85822c7bcfbf
Alexey Osipov <osipovav28@googlemail.com>
2024-07-25 13:52:34 +0300

    Create sort_an_array.py

---
+++ sort_an_array.py
@@ -0,0 +1,5 @@
+# Given an array of integers nums, sort the array in ascending order and return it.
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
