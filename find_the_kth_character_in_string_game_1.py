708683f3fa50d48d143b62839d651878a28d8e1a
Alexey Osipov <osipovav28@googlemail.com>
2025-07-03 08:59:40 +0300

    Create find_the_kth_character_in_string_game_1.py

---
+++ find_the_kth_character_in_string_game_1.py
@@ -0,0 +1,22 @@
+# Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+# You are given a positive integer k.
+# Now Bob will ask Alice to perform the following operation forever:
+# Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+# For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+# Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+# Note that the character 'z' can be changed to 'a' in the operation.
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        line = ["a"]
+        while len(line) < k:
+            line_new = []
+            for j in range(len(line)):
+                line_new.append(self.next_element(line[j]))
+            line.extend(line_new.copy())
+        return line[k - 1]
+
+    def next_element(self, elem):
+        if elem == "z":
+            return "a"
+        else:
+            return chr(ord(elem) + 1)
