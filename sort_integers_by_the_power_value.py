b93b314d0a856166d3ac1139d7d19789c24d9608
Alexey Osipov <osipovav28@googlemail.com>
2025-08-30 08:04:04 +0300

    Create sort_integers_by_the_power_value.py

---
+++ sort_integers_by_the_power_value.py
@@ -0,0 +1,25 @@
+# The power of an integer x is defined as the number of steps needed to transform x into 1 using the following steps:
+# if x is even then x = x / 2
+# if x is odd then x = 3 * x + 1
+# For example, the power of x = 3 is 7 because 3 needs 7 steps to become 1 (3 --> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1).
+# Given three integers lo, hi and k. The task is to sort all integers in the interval [lo, hi] by the power value in ascending order, if two or more integers have the same power value sort them by ascending order.
+# Return the kth integer in the range [lo, hi] sorted by the power value.
+# Notice that for any integer x (lo <= x <= hi) it is guaranteed that x will transform into 1 using these steps and that the power of x is will fit in a 32-bit signed integer.
+from functools import cache
+
+
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        return sorted(range(lo, hi + 1), key=self.calculate_power)[k - 1]
+
+    @cache
+    def calculate_power(self, num):
+        if num == 1:
+            return 0
+        else:
+            main_part, remainder = divmod(num, 2)
+            if remainder == 0:
+                num = main_part
+            else:
+                num = 3 * num + 1
+            return self.calculate_power(num) + 1
