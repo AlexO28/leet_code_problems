7c310cc7df234e27d764c91d68da9dae5a84ea13
Alexey Osipov <osipovav28@googlemail.com>
2025-08-31 10:12:39 +0300

    Update sudoku_solver.py (pass time limit checks)

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -4,37 +4,7 @@
 # Each of the digits 1-9 must occur exactly once in each column.
 # Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
 # The '.' character indicates empty cells.
-
-
-def isValid(board, row, col, c):
-    for i in range(9):
-        if board[i][col] == c or board[row][i] == c:
-          return False
-    for i in range(3):
-        for j in range(3):
-            if board[3*(row // 3) + i][3*(col // 3) + j] == c:
-                return False
-    return True
-
-
-def solve(board, i0, j0):
-    if (i0 == 9) and (j0 == 0):
-        return True
-    if j0 == 8:
-        i = i0 + 1
-        j = 0
-    else:
-        i = i0
-        j = j0 + 1
-    if board[i0][j0] != '.':
-        return solve(board, i, j)
-    for c in range(1, 10):
-        if isValid(board, i0, j0, str(c)):
-            board[i0][j0] = str(c)
-            if solve(board, i, j):
-               return True
-        board[i0][j0] = '.'
-    return False
+from typing import List
 
 
 class Solution:
@@ -42,4 +12,41 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        solve(board, 0, 0) 
+        self.row_used = [[False] * 9 for _ in range(9)]
+        self.col_used = [[False] * 9 for _ in range(9)]
+        self.block_used = [[[False] * 9 for _ in range(3)] for _ in range(3)]
+        self.empty_cells = []
+        self.board = board
+        self.solved = False
+        for row_idx in range(9):
+            for col_idx in range(9):
+                if self.board[row_idx][col_idx] == ".":
+                    self.empty_cells.append((row_idx, col_idx))
+                else:
+                    digit = int(board[row_idx][col_idx]) - 1
+                    self.row_used[row_idx][digit] = True
+                    self.col_used[col_idx][digit] = True
+                    self.block_used[row_idx // 3][col_idx // 3][digit] = True
+        self.backtrack(0)
+
+    def backtrack(self, index):
+        if index == len(self.empty_cells):
+            self.solved = True
+        else:
+            row_idx, col_idx = self.empty_cells[index]
+            for digit in range(9):
+                if (
+                    not self.row_used[row_idx][digit]
+                    and not self.col_used[col_idx][digit]
+                    and not self.block_used[row_idx // 3][col_idx // 3][digit]
+                ):
+                    self.row_used[row_idx][digit] = True
+                    self.col_used[col_idx][digit] = True
+                    self.block_used[row_idx // 3][col_idx // 3][digit] = True
+                    self.board[row_idx][col_idx] = str(digit + 1)
+                    self.backtrack(index + 1)
+                    if self.solved:
+                        break
+                    self.row_used[row_idx][digit] = False
+                    self.col_used[col_idx][digit] = False
+                    self.block_used[row_idx // 3][col_idx // 3][digit] = False
