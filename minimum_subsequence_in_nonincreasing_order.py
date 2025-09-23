6bc53ae99311980e0b2eee54b78a135d68d681c3
Alexey Osipov <osipovav28@googlemail.com>
2025-09-23 09:13:41 +0300

    Create minimum_subsequence_in_nonincreasing_order.py

---
+++ minimum_subsequence_in_nonincreasing_order.py
@@ -0,0 +1,19 @@
+# Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
+# If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
+# Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
+from typing import List
+
+
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse=True)
+        summa = sum(nums)
+        cursum = 0
+        antisum = summa
+        res = []
+        for num in nums:
+            res.append(num)
+            cursum += num
+            antisum -= num
+            if cursum > antisum:
+                return res
