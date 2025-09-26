12a023947ede5dd7c1462ca833f2f203bdcf973d
Alexey Osipov <osipovav28@googlemail.com>
2025-09-26 09:25:22 +0300

    Create number_of_students_doing_homework_at_a_given_time.py

---
+++ number_of_students_doing_homework_at_a_given_time.py
@@ -0,0 +1,17 @@
+# Given two integer arrays startTime and endTime and given an integer queryTime.
+# The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+# Return the number of students doing their homework at time queryTime. More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+from typing import List
+
+
+class Solution:
+    def busyStudent(
+        self, startTime: List[int], endTime: List[int], queryTime: int
+    ) -> int:
+        return len(
+            [
+                j
+                for j in range(len(startTime))
+                if startTime[j] <= queryTime <= endTime[j]
+            ]
+        )
