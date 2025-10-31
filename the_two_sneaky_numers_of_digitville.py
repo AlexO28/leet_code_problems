2a7e76189c4e9587fb4ef85669c38755bffb3d0c
Alexey Osipov <osipovav28@googlemail.com>
2025-10-31 09:38:32 +0300

    Create the_two_sneaky_numers_of_digitville.py

---
+++ the_two_sneaky_numers_of_digitville.py
@@ -0,0 +1,15 @@
+# In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+# As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+from typing import List
+
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        freq_dict = set()
+        res = []
+        for num in nums:
+            if num in freq_dict:
+                res.append(num)
+            else:
+                freq_dict.add(num)
+        return res
