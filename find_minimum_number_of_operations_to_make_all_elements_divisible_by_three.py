db6cc103f2c86c642bcbe669ea04e72134a6b397
Alexey Osipov <osipovav28@googlemail.com>
2025-11-22 22:46:02 +0300

    Create find_minimum_number_of_operations_to_make_all_elements_divisible_by_three.py

---
+++ find_minimum_number_of_operations_to_make_all_elements_divisible_by_three.py
@@ -0,0 +1,8 @@
+# You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+# Return the minimum number of operations to make all elements of nums divisible by 3.
+from typing import List
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        return len([num for num in nums if num % 3 != 0])
