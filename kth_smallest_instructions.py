09c43da2f18a26aa16da1361f2c7b98aeb814459
Alexey Osipov <osipovav28@googlemail.com>
2026-01-21 09:28:49 +0300

    Create kth_smallest_instructions.py

---
+++ kth_smallest_instructions.py
@@ -0,0 +1,28 @@
+# Bob is standing at cell (0, 0), and he wants to reach destination: (row, column). He can only travel right and down. You are going to help Bob by providing instructions for him to reach destination.
+# The instructions are represented as a string, where each character is either:
+# 'H', meaning move horizontally (go right), or
+# 'V', meaning move vertically (go down).
+# Multiple instructions will lead Bob to destination. For example, if destination is (2, 3), both "HHHVV" and "HVHVH" are valid instructions.
+# However, Bob is very picky. Bob has a lucky number k, and he wants the kth lexicographically smallest instructions that will lead him to destination. k is 1-indexed.
+# Given an integer array destination and an integer k, return the kth lexicographically smallest instructions that will take Bob to destination.
+from typing import List
+from math import comb
+
+
+class Solution:
+    def kthSmallestPath(self, destination: List[int], k: int) -> str:
+        v, h = destination
+        ans = []
+        for _ in range(h + v):
+            if h == 0:
+                ans.append("V")
+            else:
+                x = comb(h + v - 1, h - 1)
+                if k > x:
+                    ans.append("V")
+                    v -= 1
+                    k -= x
+                else:
+                    ans.append("H")
+                    h -= 1
+        return "".join(ans)
