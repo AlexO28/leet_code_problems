9695a2bcf23c1a80af96bb3e3a6eefe3708aa413
Alexey Osipov <osipovav28@googlemail.com>
2026-02-03 23:27:07 +0300

    Create modify_columns.py

---
+++ modify_columns.py
@@ -0,0 +1,7 @@
+# A company intends to give its employees a pay rise.
+# Write a solution to modify the salary column by multiplying each salary by 2.
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["salary"] = 2 * employees["salary"]
+    return employees
