82f20b94ad6341ca79136d7a82d00ae2dae9a550
Alexey Osipov <osipovav28@googlemail.com>
2026-02-03 23:21:33 +0300

    Create create_a_new_column.py

---
+++ create_a_new_column.py
@@ -0,0 +1,7 @@
+# A company plans to provide its employees with a bonus.
+# Write a solution to create a new column name bonus that contains the doubled values of the salary column.
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = 2 * employees["salary"]
+    return employees
