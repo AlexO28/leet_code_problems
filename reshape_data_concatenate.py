0a45da346d708d19944708fa74c49056ba879207
Alexey Osipov <osipovav28@googlemail.com>
2026-02-03 23:37:29 +0300

    Create reshape_data_concatenate.py

---
+++ reshape_data_concatenate.py
@@ -0,0 +1,6 @@
+# Write a solution to concatenate these two DataFrames vertically into one DataFrame.
+import pandas as pd
+
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2], axis=0)
