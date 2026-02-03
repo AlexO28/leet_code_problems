9779d80e390271eae522094e846ea20016143b5a
Alexey Osipov <osipovav28@googlemail.com>
2026-02-03 23:25:29 +0300

    Create drop_missing_data.py

---
+++ drop_missing_data.py
@@ -0,0 +1,6 @@
+# There are some rows having missing values in the name column.
+# Write a solution to remove the rows with missing values.
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students[~pd.isnull(students["name"])]
