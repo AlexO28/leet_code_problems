fc612c1b94ba824234e8f6d6b13dd75e9f4aafd2
Alexey Osipov <osipovav28@googlemail.com>
2026-02-03 23:57:07 +0300

    Create reshape_data_melt.py

---
+++ reshape_data_melt.py
@@ -0,0 +1,9 @@
+# Write a solution to reshape the data so that each row represents sales data for a product in a specific quarter.
+import pandas as pd
+
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars=["product"]).rename(
+        columns={"variable": "quarter", "value": "sales"}
+    )
+  
