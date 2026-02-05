37b9ed1446a15b009197f45433d947e420ddc60a
Alexey Osipov <osipovav28@googlemail.com>
2026-02-05 09:04:58 +0300

    Create find_the_most_competetive_subsequence.py

---
+++ find_the_most_competetive_subsequence.py
@@ -0,0 +1,15 @@
+# Given an integer array nums and a positive integer k, return the most competitive subsequence of nums of size k.
+# An array's subsequence is a resulting sequence obtained by erasing some (possibly zero) elements from the array.
+# We define that a subsequence a is more competitive than a subsequence b (of the same length) if in the first position where a and b differ, subsequence a has a number less than the corresponding number in b. For example, [1,3,4] is more competitive than [1,3,5] because the first position they differ is at the final number, and 4 is less than 5.
+from typing import List
+
+
+class Solution:
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        stk = []
+        for i, v in enumerate(nums):
+            while stk and (stk[-1] > v) and (len(stk) + len(nums) - i > k):
+                stk.pop()
+            if len(stk) < k:
+                stk.append(v)
+        return stk
