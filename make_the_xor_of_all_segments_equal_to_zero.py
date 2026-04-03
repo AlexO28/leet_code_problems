efed95f67a2bee1f4575f9aae8fbeef58878118b
Alexey Osipov <osipovav28@googlemail.com>
2026-04-03 09:20:04 +0300

    Create make_the_xor_of_all_segments_equal_to_zero.py

---
+++ make_the_xor_of_all_segments_equal_to_zero.py
@@ -0,0 +1,24 @@
+# You are given an array nums​​​ and an integer k​​​​​. The XOR of a segment [left, right] where left <= right is the XOR of all the elements with indices between left and right, inclusive: nums[left] XOR nums[left+1] XOR ... XOR nums[right].
+# Return the minimum number of elements to change in the array such that the XOR of all segments of size k​​​​​​ is equal to zero.
+from typing import List
+from collections import Counter
+from math import inf
+
+
+class Solution:
+    def minChanges(self, nums: List[int], k: int) -> int:
+        n = 1 << 10
+        cnt = [Counter() for i in range(k)]
+        size = [0] * k
+        for i, v in enumerate(nums):
+            cnt[i % k][v] += 1
+            size[i % k] += 1
+        f = [inf] * n
+        f[0] = 0
+        for i in range(k):
+            g = [min(f) + size[i]] * n
+            for j in range(n):
+                for v, c in cnt[i].items():
+                    g[j] = min(g[j], f[j ^ v] + size[i] - c)
+            f = g
+        return f[0]
