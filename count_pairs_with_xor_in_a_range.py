615bcc4918b0c245f1436ad02a11772436bf5650
Alexey Osipov <osipovav28@googlemail.com>
2026-04-18 09:00:35 +0300

    Create count_pairs_with_xor_in_a_range.py

---
+++ count_pairs_with_xor_in_a_range.py
@@ -0,0 +1,45 @@
+# Given a (0-indexed) integer array nums and two integers low and high, return the number of nice pairs.
+# A nice pair is a pair (i, j) where 0 <= i < j < nums.length and low <= (nums[i] XOR nums[j]) <= high.
+from typing import List
+
+
+class Solution:
+    def countPairs(self, nums: List[int], low: int, high: int) -> int:
+        if len(nums) == 1:
+            return 0
+        ans = 0
+        tree = Trie()
+        for x in nums:
+            ans += tree.search(x, high + 1) - tree.search(x, low)
+            tree.insert(x)
+        return ans
+
+
+class Trie:
+    def __init__(self):
+        self.children = [None] * 2
+        self.cnt = 0
+
+    def insert(self, x):
+        node = self
+        for i in range(15, -1, -1):
+            v = x >> i & 1
+            if node.children[v] is None:
+                node.children[v] = Trie()
+            node = node.children[v]
+            node.cnt += 1
+
+    def search(self, x, limit):
+        node = self
+        ans = 0
+        for i in range(15, -1, -1):
+            if node is None:
+                return ans
+            v = x >> i & 1
+            if limit >> i & 1:
+                if node.children[v]:
+                    ans += node.children[v].cnt
+                node = node.children[v ^ 1]
+            else:
+                node = node.children[v]
+        return ans
