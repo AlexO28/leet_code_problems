62cb77b48591de12a1feb0ae4b7df394fe9f854a
Alexey Osipov <osipovav28@googlemail.com>
2026-08-17 07:26:51 +0300

    Create a_number_after_a_double_reversal.rs

---
+++ a_number_after_a_double_reversal.rs
@@ -0,0 +1,14 @@
+/*
+Reversing an integer means to reverse all its digits.
+For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
+Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.
+*/
+impl Solution {
+    pub fn is_same_after_reversals(num: i32) -> bool {
+        if (num == 0) {
+            return true;
+        } else {
+            return num % 10 != 0;
+        }
+    }
+}
