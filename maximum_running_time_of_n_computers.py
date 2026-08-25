7f0b3009c9360b2f15ec15be7b5b8286fe8f87ee
Alexey Osipov <osipovav28@googlemail.com>
2026-08-25 06:45:10 +0300

    Create maximum_running_time_of_n_computers.py

---
+++ maximum_running_time_of_n_computers.py
@@ -0,0 +1,18 @@
+# You have n computers. You are given the integer n and a 0-indexed integer array batteries where the ith battery can run a computer for batteries[i] minutes. You are interested in running all n computers simultaneously using the given batteries.
+# Initially, you can insert at most one battery into each computer. After that and at any integer time moment, you can remove a battery from a computer and insert another battery any number of times. The inserted battery can be a totally new battery or a battery from another computer. You may assume that the removing and inserting processes take no time.
+# Note that the batteries cannot be recharged.
+# Return the maximum number of minutes you can run all the n computers simultaneously.
+from typing import List
+
+
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        left = 0
+        right = sum(batteries)
+        while left < right:
+            mid = (left + right + 1) // 2
+            if sum([min(battery, mid) for battery in batteries]) >= n * mid:
+                left = mid
+            else:
+                right = mid - 1
+        return left
