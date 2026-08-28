c52a5b7cd8df9240bb259c4d3a05ea1f6c124d86
Alexey Osipov <osipovav28@googlemail.com>
2026-08-28 17:19:40 +0300

    Create the_latest_time_to_catch_a_bus.py

---
+++ the_latest_time_to_catch_a_bus.py
@@ -0,0 +1,27 @@
+# You are given a 0-indexed integer array buses of length n, where buses[i] represents the departure time of the ith bus. You are also given a 0-indexed integer array passengers of length m, where passengers[j] represents the arrival time of the jth passenger. All bus departure times are unique. All passenger arrival times are unique.
+# You are given an integer capacity, which represents the maximum number of passengers that can get on each bus.
+# When a passenger arrives, they will wait in line for the next available bus. You can get on a bus that departs at x minutes if you arrive at y minutes where y <= x, and the bus is not full. Passengers with the earliest arrival times get on the bus first.
+# More formally when a bus arrives, either:
+# If capacity or fewer passengers are waiting for a bus, they will all get on the bus, or
+# The capacity passengers with the earliest arrival times will get on the bus.
+# Return the latest time you may arrive at the bus station to catch a bus. You cannot arrive at the same time as another passenger.
+# Note: The arrays buses and passengers are not necessarily sorted.
+from typing import List
+
+
+class Solution:
+    def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
+        buses.sort()
+        passengers.sort()
+        j = 0
+        for t in buses:
+            c = capacity
+            while c and j < len(passengers) and passengers[j] <= t:
+                c -= 1
+                j += 1
+        j -= 1
+        ans = buses[-1] if c else passengers[j]
+        while ~j and passengers[j] == ans:
+            ans -= 1
+            j -= 1
+        return ans
