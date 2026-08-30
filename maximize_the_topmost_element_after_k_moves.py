9a89cb56063652e2993e91ffbc3aa6217e5f67dd
Alexey Osipov <osipovav28@googlemail.com>
2026-08-30 21:33:49 +0300

    Create maximize_the_topmost_element_after_k_moves.py

---
+++ maximize_the_topmost_element_after_k_moves.py
@@ -0,0 +1,22 @@
+# You are given a 0-indexed integer array nums representing the contents of a pile, where nums[0] is the topmost element of the pile.
+# In one move, you can perform either of the following:
+# If the pile is not empty, remove the topmost element of the pile.
+# If there are one or more removed elements, add any one of them back onto the pile. This element becomes the new topmost element.
+# You are also given an integer k, which denotes the total number of moves to be made.
+# Return the maximum value of the topmost element of the pile possible after exactly k moves. In case it is not possible to obtain a non-empty pile after k moves, return -1.
+from typing import List
+
+
+class Solution:
+    def maximumTop(self, nums: List[int], k: int) -> int:
+        if k == 0:
+            return nums[0]
+        if len(nums) == 1:
+            if k % 2 > 0:
+                return -1
+            else:
+                return nums[0]
+        ans = max(nums[: k - 1], default=-1)
+        if k < len(nums):
+            ans = max(ans, nums[k])
+        return ans
