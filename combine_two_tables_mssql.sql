7a5f1ca3abd46577a14d34a5967de918c68a1b2f
Alexey Osipov <osipovav28@googlemail.com>
2026-08-31 20:00:07 +0300

    Create combine_two_tables_mssql.sql

---
+++ combine_two_tables_mssql.sql
@@ -0,0 +1,7 @@
+/*
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order.
+*/
+select firstName, lastName, city, "state"
+from Person as A left join "Address" as B
+on A.personId = B.personId
