4b9728315fdbed9e4fdaf2f0688d0f2bceaca003
Alexey Osipov <osipovav28@googlemail.com>
2026-09-02 06:36:09 +0300

    Create construct_uniform_parity_array_1.cs

---
+++ construct_uniform_parity_array_1.cs
@@ -0,0 +1,11 @@
+/*You are given an array nums1 of n distinct integers.
+You want to construct another array nums2 of length n such that the elements in nums2 are either all odd or all even.
+For each index i, you must choose exactly one of the following (in any order):
+nums2[i] = nums1[i]
+nums2[i] = nums1[i] - nums1[j], for an index j != i
+Return true if it is possible to construct such an array, otherwise, return false.*/
+public class Solution {
+    public bool UniformArray(int[] nums1) {
+        return true;
+    }
+}
