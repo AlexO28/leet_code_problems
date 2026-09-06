3f783abfa7fdc7dcb7c1bec7ab0280310f539220
Alexey Osipov <osipovav28@googlemail.com>
2026-09-06 07:27:21 +0300

    Create count_good_triplets_in_an_array.java

---
+++ count_good_triplets_in_an_array.java
@@ -0,0 +1,53 @@
+/*
+You are given two 0-indexed arrays nums1 and nums2 of length n, both of which are permutations of [0, 1, ..., n - 1].
+A good triplet is a set of 3 distinct values which are present in increasing order by position both in nums1 and nums2. In other words, if we consider pos1v as the index of the value v in nums1 and pos2v as the index of the value v in nums2, then a good triplet will be a set (x, y, z) where 0 <= x, y, z <= n - 1, such that pos1x < pos1y < pos1z and pos2x < pos2y < pos2z.
+Return the total number of good triplets.
+*/
+class Solution {
+    public long goodTriplets(int[] nums1, int[] nums2) {
+        int[] pos = new int[nums1.length];
+        BinaryIndexedTree tree = new BinaryIndexedTree(nums1.length);
+        for (int i = 0; i < nums1.length; ++i) {
+            pos[nums2[i]] = i + 1;
+        }
+        long ans = 0;
+        for (int num : nums1) {
+            int p = pos[num];
+            long left = tree.query(p);
+            long right = nums1.length - p - (tree.query(nums1.length) - tree.query(p));
+            ans += left * right;
+            tree.update(p, 1);
+        }
+        return ans;
+    }
+}
+
+class BinaryIndexedTree {
+    private int n;
+    private int[] c;
+
+    public BinaryIndexedTree(int n) {
+        this.n = n;
+        c = new int[n + 1];
+    }
+
+    public void update(int x, int delta) {
+        while (x <= n) {
+            c[x] += delta;
+            x += lowbit(x);
+        }
+    }
+
+    public int query(int x) {
+        int s = 0;
+        while (x > 0) {
+            s += c[x];
+            x -= lowbit(x);
+        }
+        return s;
+    }
+
+    public static int lowbit(int x) {
+        return x & -x;
+    }
+}
