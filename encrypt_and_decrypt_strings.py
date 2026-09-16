ff9e83e98fd9313f2f63c57d5175669c32700d73
Alexey Osipov <osipovav28@googlemail.com>
2026-09-16 09:32:02 +0300

    Create encrypt_and_decrypt_strings.py

---
+++ encrypt_and_decrypt_strings.py
@@ -0,0 +1,39 @@
+# You are given a character array keys containing unique characters and a string array values containing strings of length 2. You are also given another string array dictionary that contains all permitted original strings after decryption. You should implement a data structure that can encrypt or decrypt a 0-indexed string.
+# A string is encrypted with the following process:
+# For each character c in the string, we find the index i satisfying keys[i] == c in keys.
+# Replace c with values[i] in the string.
+# Note that in case a character of the string is not present in keys, the encryption process cannot be carried out, and an empty string "" is returned.
+# A string is decrypted with the following process:
+# For each substring s of length 2 occurring at an even index in the string, we find an i such that values[i] == s. If there are multiple valid i, we choose any one of them. This means a string could have multiple possible strings it can decrypt to.
+# Replace s with keys[i] in the string.
+# Implement the Encrypter class:
+# Encrypter(char[] keys, String[] values, String[] dictionary) Initializes the Encrypter class with keys, values, and dictionary.
+# String encrypt(String word1) Encrypts word1 with the encryption process described above and returns the encrypted string.
+# int decrypt(String word2) Returns the number of possible strings word2 could decrypt to that also appear in dictionary.
+from typing import List
+from collections import Counter
+
+
+class Encrypter:
+
+    def __init__(self, keys: List[str], values: List[str], dictionary: List[str]):
+        self.map = dict(zip(keys, values))
+        self.counter = Counter([self.encrypt(v) for v in dictionary])
+
+    def encrypt(self, word1: str) -> str:
+        res = []
+        for c in word1:
+            if c not in self.map:
+                return ""
+            else:
+                res.append(self.map[c])
+        return "".join(res)
+
+    def decrypt(self, word2: str) -> int:
+        return self.counter[word2]
+
+
+# Your Encrypter object will be instantiated and called as such:
+# obj = Encrypter(keys, values, dictionary)
+# param_1 = obj.encrypt(word1)
+# param_2 = obj.decrypt(word2)
