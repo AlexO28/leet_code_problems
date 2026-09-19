1a657a7d7f83d397753528f008a7f9f1e575730d
Alexey Osipov <osipovav28@googlemail.com>
2026-09-19 07:03:56 +0300

    Create maximum_consecutive_floors_without_special_floor.py

---
+++ maximum_consecutive_floors_without_special_floor.py
@@ -0,0 +1,11 @@
+# Alice manages a company and has rented some floors of a building as office space. Alice has decided some of these floors should be special floors, used for relaxation only.
+# You are given two integers bottom and top, which denote that Alice has rented all the floors from bottom to top (inclusive). You are also given the integer array special, where special[i] denotes a special floor that Alice has designated for relaxation.
+# Return the maximum number of consecutive floors without a special floor.
+class Solution:
+    def maxConsecutive(self, bottom: int, top: int, special: list[int]) -> int:
+        special.sort()
+        res = max(special[0] - bottom, top - special[-1])
+        if len(special) > 1:
+            for i in range(1, len(special)):
+                res = max(res, special[i] - special[i - 1] - 1)
+        return res
