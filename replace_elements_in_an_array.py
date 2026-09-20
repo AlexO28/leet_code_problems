7e431fbd3fdd69dc78863bdc8ea1896812ce0afa
Alexey Osipov <osipovav28@googlemail.com>
2026-09-20 10:19:06 +0300

    Create replace_elements_in_an_array.py

---
+++ replace_elements_in_an_array.py
@@ -0,0 +1,12 @@
+# You are given a 0-indexed array nums that consists of n distinct positive integers. Apply m operations to this array, where in the ith operation you replace the number operations[i][0] with operations[i][1].
+# It is guaranteed that in the ith operation:
+# operations[i][0] exists in nums.
+# operations[i][1] does not exist in nums.
+# Return the array obtained after applying all the operations.
+class Solution:
+    def arrayChange(self, nums: list[int], operations: list[list[int]]) -> list[int]:
+        nums_dict = {nums[i] : i for i in range(len(nums))}
+        for operation in operations:
+            nums_dict[operation[1]] = nums_dict[operation[0]]
+            del nums_dict[operation[0]]
+        return sorted(nums_dict, key=lambda k: nums_dict[k])
