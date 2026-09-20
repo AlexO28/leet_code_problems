8e3b6a6ab21a296bbd87a38284c9d942a50e5f0f
Alexey Osipov <osipovav28@googlemail.com>
2026-09-20 09:45:54 +0300

    Create reverse_degree_of_a_string.cs

---
+++ reverse_degree_of_a_string.cs
@@ -0,0 +1,16 @@
+/*
+Given a string s, calculate its reverse degree.
+The reverse degree is calculated as follows:
+For each character, multiply its position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1) with its position in the string (1-indexed).
+Sum these products for all characters in the string.
+Return the reverse degree of s.
+*/
+public class Solution {
+    public int ReverseDegree(string s) {
+        int reverseDegree = 0;
+        for (int i = 0; i < s.Length; ++i) {
+            reverseDegree += (26 - (s[i] - 'a')) * (i + 1);
+        }
+        return reverseDegree;
+    }
+}
